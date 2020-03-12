472ddd9da2d83447fba641e167bea2c5605381ab
Andriy Orehov <andriyorehov@gmail.com>
2020-03-12 15:38:53 +0200

    delete old docs

--- docs/src/bootstrap.aot.ts
+++
@@ -1,7 +0,0 @@
-import 'reflect-metadata';
-import {enableProdMode} from '@angular/core';
-import {platformBrowser} from '@angular/platform-browser';
-// import {DemoModuleNgFactory} from '../ngfactory/demo/src/demo.module.ngfactory';
-// Disabled for now since it is buggy to build.
-enableProdMode();
-platformBrowser().bootstrapModuleFactory(DemoModuleNgFactory);
--- docs/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { DemoModule } from './demo.module';
-
-platformBrowserDynamic().bootstrapModule(DemoModule)
-  .catch(err => console.error(err));
